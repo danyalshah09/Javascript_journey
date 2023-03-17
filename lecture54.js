802dbeec0138d8a4b0b233f1e3f499b30c685e69
danyal shah <119670767+danyalthewebdev@users.noreply.github.com>
2023-03-18 02:59:21 +0500

    Add files via upload

---
+++ lecture54.js
@@ -0,0 +1,20 @@
+//I literally found this concept very interesting
+
+        //Interactions using alert
+//Alert returns undefined means it returns nothing 
+// alert("Congratulations! You have won an Iphone 14 pro max");
+
+//stored whatever the user typed in the prompt
+//let name= prompt("Give me Your Identity","Password");
+//console.log(name)   
+
+
+let checkage = prompt("Wait! Enter your age to enter the club")
+console.log(checkage)
+  
+if(checkage>18){
+         alert("Welcome to the club BRO")
+            }
+            else{
+          alert("You are still a kid man you cannot enter the CLUB")
+ }
