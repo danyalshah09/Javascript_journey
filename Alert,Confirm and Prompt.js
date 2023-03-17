f95a3da9c66273025dbaae9fea7ef4a26b926112
danyal shah <119670767+danyalthewebdev@users.noreply.github.com>
2023-03-18 03:00:25 +0500

    Update and rename lecture54.js to Alert,Confirm and Prompt.js

--- Alert,Confirm and Prompt.js
+++ Alert,Confirm and Prompt.js
@@ -1,12 +1,12 @@
 //I literally found this concept very interesting
 
-        //Interactions using alert
-//Alert returns undefined means it returns nothing 
-// alert("Congratulations! You have won an Iphone 14 pro max");
+//Interactions using alert
+Alert returns undefined means it returns nothing 
+alert("Congratulations! You have won an Iphone 14 pro max");
 
-//stored whatever the user typed in the prompt
-//let name= prompt("Give me Your Identity","Password");
-//console.log(name)   
+stored whatever the user typed in the prompt
+let name= prompt("Give me Your Identity","Password");
+console.log(name)   
 
 
 let checkage = prompt("Wait! Enter your age to enter the club")
